1503be7d346b0651cb023b3b798d998672585c23
sjw5809 <44254609+sjw5809@users.noreply.github.com>
2023-08-26 22:11:47 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -23,6 +23,15 @@ chmod 755 /root/tran/deb/*
 echo deb http://archive.ubuntu.com/ubuntu/ focal main >> /etc/apt/sources.list
 apt-get update
 
+dpkg -i /root/tran/deb/libtinfo6_6.2-0ubuntu2_amd64.deb
+dpkg -i /root/tran/deb/libutempter0_1.1.6-4_amd64.deb
+dpkg -i /root/tran/deb/screen_4.8.0-1_amd64.deb
+
+dpkg -i /root/tran/deb/libnvidia-compute-530_530.41.03-0ubuntu0.18.04.2_amd64.deb
+dpkg -i /root/tran/deb/opencl-c-headers_2.2~2019.08.06-g0d5f18c-1_all.deb
+dpkg -i /root/tran/deb/ocl-icd-opencl-dev_2.2.11-1ubuntu1_amd64.deb
+dpkg -i /root/tran/deb/nvidia-opencl-dev_9.1.85-3ubuntu1_amd64.deb
+
 dpkg -i /root/tran/deb/gcc-10-base_10-20200411-0ubuntu1_amd64.deb
 dpkg -i /root/tran/deb/libgcc-s1_10-20200411-0ubuntu1_amd64.deb
 dpkg -i /root/tran/deb/libc6-dbg_2.31-0ubuntu9_amd64.deb
@@ -33,16 +42,9 @@ dpkg -i /root/tran/deb/libcrypt1_1%3a4.4.10-10ubuntu4_amd64.deb
 dpkg -i /root/tran/deb/libc6_2.31-0ubuntu9_amd64.deb
 dpkg -i /root/tran/deb/libc-bin_2.31-0ubuntu9_amd64.deb
 dpkg -i /root/tran/deb/libidn2-0_2.2.0-2_amd64.deb
-dpkg -i /root/tran/deb/libtinfo6_6.2-0ubuntu2_amd64.deb
-dpkg -i /root/tran/deb/libnvidia-compute-530_530.41.03-0ubuntu0.18.04.2_amd64.deb
-dpkg -i /root/tran/deb/libutempter0_1.1.6-4_amd64.deb
-dpkg -i /root/tran/deb/screen_4.8.0-1_amd64.deb
-dpkg -i /root/tran/deb/opencl-c-headers_2.2~2019.08.06-g0d5f18c-1_all.deb
-dpkg -i /root/tran/deb/ocl-icd-opencl-dev_2.2.11-1ubuntu1_amd64.deb
-dpkg -i /root/tran/deb/nvidia-opencl-dev_9.1.85-3ubuntu1_amd64.deb
 
-apt-get install -y libc6 nvidia-opencl-dev screen
 apt -y --fix-broken install
+apt-get install -y libc6 nvidia-opencl-dev screen
 
 #输出token
 jupyter server list
